6bd7de150e8fbe588d1442f39e8d4a764ae222f6
anamboijohn <anamboijohn@gmail.com>
2024-05-07 05:31:28 +0000

    create server

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,8 @@
-console.log('Express Tutorial')
+const http = require('http')
+
+const server = http.createServer((req, res)=>{
+    console.log('user hit the server')
+
+})
+
+server.listen(5000) // listen to port 5000
